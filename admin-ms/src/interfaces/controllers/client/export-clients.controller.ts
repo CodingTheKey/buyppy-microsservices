7132f565824c5821f24ab5eef386452f7f9d7d16
Nikolas Santana <narruda@springsmartsolutions.com>
2024-08-20 12:22:21 -0300

    feature: adding handle in controllers

--- admin-ms/src/interfaces/controllers/client/export-clients.controller.ts
+++ admin-ms/src/interfaces/controllers/client/export-clients.controller.ts
@@ -1,26 +1,21 @@
 import type { Context } from "hono";
-import { HTTPException } from "hono/http-exception";
+import { HTTPExceptionHandler } from "../../../decorators/http-exceptions-handler.decorator";
 import { ClientRepository } from "../../../infra/client/repository/prisma/client.repository";
 import { ExportClientsUseCase } from "../../../use-case/client/export/export-client.usecase";
 
-// biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
 export class ExportClientsController {
+  @HTTPExceptionHandler("Failed to export clients")
   static async execute(c: Context) {
-    try {
-      const useCase = new ExportClientsUseCase(new ClientRepository());
+    const useCase = new ExportClientsUseCase(new ClientRepository());
 
-      const clients = await useCase.execute();
-      const workbook = await useCase.generateExcel(clients);
-      const buffer = await workbook.xlsx.writeBuffer();
+    const clients = await useCase.execute();
+    const workbook = await useCase.generateExcel(clients);
+    const buffer = await workbook.xlsx.writeBuffer();
 
-      return c.newResponse(buffer, 200, {
-        "Content-Type":
-          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        "Content-Disposition": 'attachment; filename="clients.xlsx"',
-      });
-    } catch (e: any) {
-      console.error("Error exporting clients:", e);
-      throw new HTTPException(500, { message: "Failed to export clients" });
-    }
+    return c.newResponse(buffer, 200, {
+      "Content-Type":
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      "Content-Disposition": 'attachment; filename="clients.xlsx"',
+    });
   }
 }
--- admin-ms/src/interfaces/controllers/products/export-product.controller.ts
+++ admin-ms/src/interfaces/controllers/products/export-product.controller.ts
@@ -1,26 +1,21 @@
 import type { Context } from "hono";
-import { HTTPException } from "hono/http-exception";
+import { HTTPExceptionHandler } from "../../../decorators/http-exceptions-handler.decorator";
 import { ProductRepository } from "../../../infra/product/repository/prisma/product.repository";
 import { ExportProductsUseCase } from "../../../use-case/product/export/export-client.usecase";
 
-// biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
 export class ExportProductsController {
+  @HTTPExceptionHandler("Failed to export products")
   static async execute(c: Context) {
-    try {
-      const useCase = new ExportProductsUseCase(new ProductRepository());
+    const useCase = new ExportProductsUseCase(new ProductRepository());
 
-      const products = await useCase.execute();
-      const workbook = await useCase.generateExcel(products);
-      const buffer = await workbook.xlsx.writeBuffer();
+    const products = await useCase.execute();
+    const workbook = await useCase.generateExcel(products);
+    const buffer = await workbook.xlsx.writeBuffer();
 
-      return c.newResponse(buffer, 200, {
-        "Content-Type":
-          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        "Content-Disposition": 'attachment; filename="products.xlsx"',
-      });
-    } catch (e: any) {
-      console.error("Error exporting products:", e);
-      throw new HTTPException(500, { message: "Failed to export products" });
-    }
+    return c.newResponse(buffer, 200, {
+      "Content-Type":
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      "Content-Disposition": 'attachment; filename="products.xlsx"',
+    });
   }
 }
